d9651126a9359edebe70ad144c35f942d0debc81
Zaverie <zaveriefofou9@gmail.com>
2023-04-25 16:02:25 +0200

    Added  authentification and Models for tables

---
+++ app/Models/Entrées/Sorties.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Entrées;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sorties extends Model
+{
+    use HasFactory;
+}
